81daf2f3748869eb5b22e10c6895d8b18b1d9001
John <johnbt@runbox.com>
2022-07-12 14:49:22 -0600

    Streamlit app

--- compute_edge_image.py
+++ compute_edge_image.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import signal, misc
 from copy import deepcopy
+import scipy
 
 def compute_deriv(image = None):
     '''
@@ -30,10 +31,36 @@ def compute_deriv(image = None):
 
     return final
 
+import streamlit as st
+from PIL import Image, ImageOps
+
+def st_ui():
+    '''
+    Function running the Streamlit UI.
+    Doesn't return anything.
+    '''
+    
+    st.set_page_config(layout = "wide")
+    st.title("Compute edges")
+
+    user_image = st.sidebar.file_uploader("Load your own image")
+    if user_image is not None:
+        im = Image.open(user_image)
+        imagegray = ImageOps.grayscale(im)
+        image = np.array(imagegray).astype(np.float32)
+    
+    else:
+        im = scipy.misc.face()
+        image = misc.face(gray=True).astype(np.float32)
+
+    st.header("Original image")
+    st.image(im)
+
+    final = compute_deriv(image)
+    
+    st.header("Edge image")
+    st.image(final)
+
+
 if __name__ == "__main__":
-    edge_image = compute_deriv()
-
-    import matplotlib.pyplot as plt
-
-    plt.imshow(edge_image)
-    plt.show()
+    st_ui()
